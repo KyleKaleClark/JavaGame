aee89801d9cad77d71b10ec41292e3fe06d8eae4
Kyle_Clark <kjclark1998@gmail.com>
2022-08-03 13:52:47 -0700

    updated method names and added Direction variable
    yuh

--- core/src/com/mygdx/game/Rabbit.java
+++ core/src/com/mygdx/game/Rabbit.java
@@ -3,21 +3,23 @@ package com.mygdx.game;
 public class Rabbit {
 
     private float xLoc, yLoc;
+    private int dir;
     private String img;
 
-    public Rabbit(float x, float y, String img_location){
+    public Rabbit(float x, float y, int direction, String img_location){
         this.xLoc = x;
         this.yLoc = y;
         this.img = img_location;
+        this.dir = direction;
     }
 
-    public float returnX(){
+    public float getX(){
         return xLoc;
     }
     public void setX(float updatedx){
         xLoc = updatedx;
     }
-    public float returnY(){
+    public float getY(){
         return yLoc;
     }
     public void setY(float updatedy){
@@ -29,4 +31,11 @@ public class Rabbit {
     public void updatefile(String updatedimg){
         img = updatedimg;
     }
+    public void setDir(int updatedDir){
+        dir = updatedDir;
+    }
+    public void getDir(){
+        return dir;
+    }
+
 }
